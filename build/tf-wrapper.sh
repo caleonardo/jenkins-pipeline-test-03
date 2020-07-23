8106b2c1d1e86dc49d28eebed41cefc21d2de686
caleonardo <65500442+caleonardo@users.noreply.github.com>
2020-07-23 16:59:15 +0100

    testing Jenkins pipeline - Added LATEST tf-wrapper - 02

--- build/tf-wrapper.sh
+++ build/tf-wrapper.sh
@@ -13,67 +13,38 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-echo "STARTING tf-wrapper HERE-"
 
-branchname=$2
+action=$1
+branch=$2
 policyrepo=$3
+base_dir=$(pwd)
+environments_regex="^(dev|nonprod|prod|shared)$"
 
-#tf_validator_path="./terraform-validator-linux-amd64"
-tf_validator_path="/usr/local/bin/terraform-validator"
-
-##functions
+## Terraform apply for single environment.
 tf_apply() {
-  terraform apply -input=false -auto-approve "${branchname}.tfplan" || exit 1
-}
-
-tf_init() {
-  terraform init || exit 11
-}
-
-tf_plan() {
-  terraform plan -input=false -out "${branchname}.tfplan" || exit 21
-}
-
-tf_validate() {
-  if ! ${tf_validator_path} version &> /dev/null; then
-    echo "terraform-validator not found!  Check path or visit"
-    echo "https://github.com/forseti-security/policy-library/blob/master/docs/user_guide.md#how-to-use-terraform-validator"
+  local path=$1
+  local tf_env=$2
+  echo "*************** TERRAFORM APPLY *******************"
+  echo "      At environment: ${tf_env} "
+  echo "***************************************************"
+  if [ -d "$path" ]; then
+    cd "$path" || exit
+    terraform apply -input=false -auto-approve "${tf_env}.tfplan" || exit 1
+    cd "$base_dir" || exit
   else
-    terraform show -json "${branchname}.tfplan" > "${branchname}.json" || exit 32
-    ${tf_validator_path} validate "${branchname}.json" --policy-path="${policyrepo}" || exit 33
+    echo "ERROR:  ${path} does not exist"
   fi
 }
 
-##main
-case "$1" in
-  apply )
-    echo "*************** TERRAFORM APPLY *******************"
-    echo "      At environment: ${branchname} "
-    echo "***************************************************"
-
-    cd ../1-org/envs/shared || exit 44
-    # cd "./envs/${branchname}" || exit 44
-
-    if [ ! -d ".terraform" ]; then
-      tf_init
-    fi
-
-    if [ ! -f "${branchname}.tfplan" ]; then
-      tf_plan
-    fi
-
-    tf_validate
-
-    tf_apply
-    ;;
-
-  init )
-    echo "*************** TERRAFORM INIT *******************"
-    echo "      At environment: ${branchname} "
-    echo "**************************************************"
-
-     echo "1 - WHAT DIRECTORY ARE WE IN?"
-     echo "`pwd`"
+## terraform init for single environment.
+tf_init() {
+  local path=$1
+  local tf_env=$2
+  echo "*************** TERRAFORM INIT *******************"
+  echo "      At environment: ${tf_env} "
+  echo "**************************************************"
+    echo "1 - WHAT DIRECTORY ARE WE IN?"
+    echo "`pwd`"
     echo "1 - -----------------------------------"
 
     echo "2 - SEE THE OUTPUT FOR gcloud auth list"
@@ -92,46 +63,118 @@ case "$1" in
     gcloud auth list
     echo "4 - -----------------------------------"
 
-    cd ../1-org/envs/shared || exit 44
-    # cd "./envs/${branchname}" || exit 44
+  if [ -d "$path" ]; then
+    cd "$path" || exit
+    terraform init || exit 11
+    cd "$base_dir" || exit
+  else
+    echo "ERROR:  ${path} does not exist"
+  fi
+}
 
-    tf_init
+## terraform plan for single environment.
+tf_plan() {
+  local path=$1
+  local tf_env=$2
+  echo "*************** TERRAFORM PLAN *******************"
+  echo "      At environment: ${tf_env} "
+  echo "**************************************************"
+  if [ -d "$path" ]; then
+    cd "$path" || exit
+    terraform plan -input=false -out "${tf_env}.tfplan" || exit 21
+    cd "$base_dir" || exit
+  else
+    echo "ERROR:  ${tf_env} does not exist"
+  fi
+}
+
+## terraform init/plan for all valid environments matching regex.
+tf_init_plan_all() {
+  # shellcheck disable=SC2012
+  ls "$base_dir" | while read -r component ; do
+    # shellcheck disable=SC2012
+    ls "$base_dir/$component" | while read -r env ; do
+      if [[ "$env" =~ $environments_regex ]] ; then
+       tf_dir="$base_dir/$component/$env"
+       tf_init "$tf_dir" "$env"
+       tf_plan "$tf_dir" "$env"
+      else
+        echo "$component/$env doesn't match $environments_regex; skipping"
+      fi
+    done
+  done
+}
+
+## terraform validate for single environment.
+tf_validate() {
+  local path=$1
+  local tf_env=$2
+  local policy_file_path=$3
+  echo "*************** TERRAFORM VALIDATE ******************"
+  echo "      At environment: ${tf_env} "
+  echo "      Using policy from: ${policy_file_path} "
+  echo "*****************************************************"
+  if ! command -v terraform-validator &> /dev/null; then
+    echo "terraform-validator not found!  Check path or visit"
+    echo "https://github.com/forseti-security/policy-library/blob/master/docs/user_guide.md#how-to-use-terraform-validator"
+  else
+    if [ -d "$path" ]; then
+      cd "$path" || exit
+      terraform show -json "${tf_env}.tfplan" > "${tf_env}.json" || exit 32
+      terraform-validator validate "${tf_env}.json" --policy-path="${policy_file_path}" || exit 33
+      cd "$base_dir" || exit
+    else
+      echo "ERROR:  ${path} does not exist"
+    fi
+  fi
+}
+
+# Runs single action for each instance of env in folder hierarchy.
+single_action_runner() {
+  # shellcheck disable=SC2012
+  ls "$base_dir" | while read -r component ; do
+    # sort -r is added to ensure shared is first if it exists.
+    # shellcheck disable=SC2012
+    ls "$base_dir/$component" | sort -r | while read -r env ; do
+      # perform action only if folder matches branch OR folder is shared & branch is prod.
+      if [[ "$env" == "$branch" ]] || [[ "$env" == "shared" && "$branch" == "prod" ]]; then
+        tf_dir="$base_dir/$component/$env"
+        case "$action" in
+          apply )
+            tf_apply "$tf_dir" "$env"
+            ;;
+
+          init )
+            tf_init "$tf_dir" "$env"
+            ;;
+
+          plan )
+            tf_plan "$tf_dir" "$env"
+            ;;
+
+          validate )
+            tf_validate "$tf_dir" "$env" "$policyrepo"
+            ;;
+          * )
+            echo "unknown option: ${action}"
+            ;;
+        esac
+      else
+        echo "${env} doesn't match ${branch}; skipping"
+      fi
+    done
+  done
+}
+
+case "$action" in
+  init|plan|apply|validate )
+    single_action_runner
     ;;
 
-  plan )
-    echo "*************** TERRAFORM PLAN *******************"
-    echo "      At environment: ${branchname} "
-    echo "**************************************************"
-
-    cd ../1-org/envs/shared || exit 44
-    # cd "./envs/${branchname}" || exit 44
-
-    if [ ! -d ".terraform" ]; then
-      tf_init
-    fi
-
-    tf_plan
+  planall )
+    tf_init_plan_all
     ;;
 
-  validate )
-    echo "*************** TERRAFORM VALIDATE ******************"
-    echo "      At environment: ${branchname} "
-    echo "      Using policy from: ${policyrepo} "
-    echo "****************************************************"
-
-    cd ../1-org/envs/shared || exit 44
-    # cd "./envs/${branchname}" || exit 44
-
-    if [ ! -d ".terraform" ]; then
-      tf_init
-    fi
-
-    if [ ! -f "${branchname}.tfplan" ]; then
-      tf_plan
-    fi
-
-    tf_validate
-    ;;
   * )
     echo "unknown option: ${1}"
     exit 99
