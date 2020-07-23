2e23af40410138669d45cf6821e348f3fab07c43
caleonardo <65500442+caleonardo@users.noreply.github.com>
2020-07-23 17:55:31 +0100

    Update tf-wrapper.sh

--- build/tf-wrapper.sh
+++ build/tf-wrapper.sh
@@ -17,7 +17,7 @@
 action=$1
 branch=$2
 policyrepo=$3
-cd ..  # make base_dir /home/jenkins/jenkins_agent_dir/workspace/pipeline-7-multibranch_develop/ instead of /home/jenkins/jenkins_agent_dir/workspace/pipeline-7-multibranch_develop/build
+#cd ..  # make base_dir /home/jenkins/jenkins_agent_dir/workspace/pipeline-7-multibranch_develop/ instead of /home/jenkins/jenkins_agent_dir/workspace/pipeline-7-multibranch_develop/build
 base_dir=$(pwd)
 environments_regex="^(dev|nonprod|prod|shared)$"
 
